73db3bb68e02572a30c934db7b65bad0cd6c266e
Joelthomas62384 <103513948+Joelthomas62384@users.noreply.github.com>
2025-02-22 08:41:33 +0530

    Update middlewares.py

---
+++ app/middlewares.py
@@ -0,0 +1,20 @@
+from django.utils.deprecation import MiddlewareMixin
+from django.http import JsonResponse
+from .models import Tenants
+
+class TenantMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        path = request.path.strip('/').split('/')
+        
+        if len(path) < 2:
+            return JsonResponse({'error': 'Invalid request path'}, status=400)
+        
+        service, tenant_subdomain, *newpath = path
+
+        tenant = Tenants.objects.filter(subdomain=tenant_subdomain).first()
+        if not tenant:
+            return JsonResponse({'error': 'Invalid tenant'}, status=404)
+
+        request.tenant = tenant
+        request.path = "/" + "/".join(newpath)  
+
